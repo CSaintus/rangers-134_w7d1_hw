b0619ac361d2650e819b99b0aad158eb427906dd
Claudel Saintus <claudelsaintus@Claudels-Mac-mini.local>
2023-11-21 10:09:24 -0500

    w7d1 hw

---
+++ day_one.js
@@ -0,0 +1,18 @@
+//==================Exercise #1 ==========//
+/*Write a function that takes in the string and the list of dog names, loops through 
+the list and checks that the current name is in the string passed in. The output should be:
+"Matched dog_name" if name is in the string, if no matches are present console.log "No Matches"
+For extra credit, output "Thats a damn cat" 
+*/
+
+let dogString = "All dogs are good boys. Dogs named Precious can be annoying but are still good. Dogs named Shadow are usually majestic and cool.\
+Sassy is a cat, but Chance is a dog. Can cat people be trusted? I don't know. Also Potato is a great dog name for a lil tiny stubby guy."
+
+let dogNames = ["Fido", "Precious", "Sassy", "Gertrude", "Shadow", "Potato", "Bart"]
+
+//============Exercise #2 ============//
+/*Write a fucntion that takes in an array and removes every even index with a splice,
+and replaces it with the string "even index". For bonus points instead of inserting even index
+replace each character with their signature move.*/
+
+arr == ["Goku", "Vegeta", "Trunks", "Krillin", "Gohan", "Piccolo"]
